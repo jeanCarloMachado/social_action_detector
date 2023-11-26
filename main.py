99baab530317e82d738ba7095c1ff6c311356b84
Jean Carlo Machado <jean.machado@getyourguide.com>
2023-11-26 18:24:56 +0100

    test

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 
 import torch
 import torch.nn as nn
+from torch.utils.data import DataLoader
 from transformers import AutoModel
 
 class BinaryClassification(nn.Module):
@@ -18,7 +19,32 @@ class BinaryClassification(nn.Module):
 
        return outputs
 
+# Training loop
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model = BinaryClassification().to(device)
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
 
-if __name__ == "__main__":
-    import fire
-    fire.Fire()
+train_dataloader = DataLoader(train_dataset, batch_size=32, shuffle=True)
+
+for epoch in range(num_epochs):
+    model.train()
+    total_loss = 0
+
+    for batch in train_dataloader:
+        input_ids = batch['input_ids'].to(device)
+        attn_mask = batch['attention_mask'].to(device)
+        labels = batch['labels'].to(device)
+
+        optimizer.zero_grad()
+        outputs = model(input_ids, attn_mask)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        total_loss += loss.item()
+
+    avg_loss = total_loss / len(train_dataloader)
+    print(f"Epoch {epoch+1}/{num_epochs}, Average Loss: {avg_loss}")
+
+# END: abpxx6d04wxr
