b71cdb4c39c302bb12e51ffc7c3da432f6873310
Jean Carlo Machado <jean.machado@getyourguide.com>
2023-11-26 19:01:38 +0100

    add compute metric

--- main.py
+++ main.py
@@ -1,50 +1,91 @@
 
 import torch
 import torch.nn as nn
-from torch.utils.data import DataLoader
-from transformers import AutoModel
+from transformers import AutoModel, AutoTokenizer, Trainer, TrainingArguments
+from datasets import load_dataset
+import pandas as pd
+from datasets import Dataset
+from sklearn.metrics import accuracy_score
 
+# Define the model
 class BinaryClassification(nn.Module):
-   def __init__(self):
-       super(BinaryClassification, self).__init__()
-       self.base_model = AutoModel.from_pretrained('bert-base-uncased')
-       self.dropout = nn.Dropout(0.5)
-       self.linear = nn.Linear(768, 2)
+    def __init__(self):
+        super(BinaryClassification, self).__init__()
+        self.base_model = AutoModel.from_pretrained('bert-base-uncased')
+        self.dropout = nn.Dropout(0.5)
+        self.linear = nn.Linear(768, 2)
 
+    def forward(self, input_ids, attention_mask):
+        outputs = self.base_model(input_ids, attention_mask=attention_mask)
+        outputs = self.dropout(outputs[0])
+        outputs = self.linear(outputs)
+        return outputs
 
-   def forward(self, input_ids, attn_mask):
-       outputs = self.base_model(input_ids, attention_mask=attn_mask)
-       outputs = self.dropout(outputs[0])
-       outputs = self.linear(outputs)
+# Load the dataset from the CSV file
+data = pd.read_csv('data.csv')
+descriptions = data['description'].tolist()
+labels = data['label'].tolist()
 
-       return outputs
+# Tokenize the input data
+tokenizer = AutoTokenizer.from_pretrained('bert-base-uncased')
+tokenized_inputs = tokenizer(descriptions, padding=True, truncation=True, max_length=512, return_tensors='pt')
 
-# Training loop
+# Prepare the dataset
+input_ids = tokenized_inputs['input_ids']
+attention_mask = tokenized_inputs['attention_mask']
+
+# Create a dictionary with the input data
+data_dict = {
+    'input_ids': input_ids,
+    'attention_mask': attention_mask,
+    'labels': labels
+}
+
+# Create the dataset
+dataset = Dataset.from_dict(data_dict)
+
+# Split the dataset into training and validation sets
+train_size = int(0.8 * len(dataset))
+train_dataset, val_dataset = torch.utils.data.random_split(dataset, [train_size, len(dataset) - train_size])
+
+# Define the model, optimizer, and loss function
+model = BinaryClassification()
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-model = BinaryClassification().to(device)
-criterion = nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+model.to(device)
+optimizer = torch.optim.AdamW(model.parameters(), lr=5e-5)
+loss_fn = nn.CrossEntropyLoss()
 
-train_dataloader = DataLoader(train_dataset, batch_size=32, shuffle=True)
+# Define the training arguments
+training_args = TrainingArguments(
+    output_dir='./results',
+    num_train_epochs=2,
+    per_device_train_batch_size=16,
+    per_device_eval_batch_size=16,
+    warmup_steps=500,
+    weight_decay=0.01,
+    logging_dir='./logs',
+    logging_steps=10,
+    evaluation_strategy='epoch'
+)
 
-for epoch in range(num_epochs):
-    model.train()
-    total_loss = 0
 
-    for batch in train_dataloader:
-        input_ids = batch['input_ids'].to(device)
-        attn_mask = batch['attention_mask'].to(device)
-        labels = batch['labels'].to(device)
+def compute_metrics(pred):
+    labels = pred.label_ids
+    preds = pred.predictions.argmax(-1)
+    acc = accuracy_score(labels, preds)
+    return {"accuracy": acc}
 
-        optimizer.zero_grad()
-        outputs = model(input_ids, attn_mask)
-        loss = criterion(outputs, labels)
-        loss.backward()
-        optimizer.step()
+trainer = Trainer(
+    model=model,
+    args=training_args,
+    train_dataset=train_dataset,
+    eval_dataset=val_dataset,
+    compute_metrics=compute_metrics,
+    pcompute_metrics=None,
+    optimizers=(optimizer, None),
+    data_collator=None,
+    tokenizer=tokenizer
+)
 
-        total_loss += loss.item()
+trainer.train()
 
-    avg_loss = total_loss / len(train_dataloader)
-    print(f"Epoch {epoch+1}/{num_epochs}, Average Loss: {avg_loss}")
-
-# END: abpxx6d04wxr
