2426e5855d94e4e09f350688e68f86b7cacbd1bb
Jean Carlo Machado <jean.machado@getyourguide.com>
2023-11-30 20:24:26 +0100

    add entries

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 
 import torch
-import torch.nn as nn
-from transformers import AutoModel, AutoTokenizer, Trainer, TrainingArguments
+from transformers import AutoTokenizer, Trainer, TrainingArguments
 from datasets import load_dataset
 import pandas as pd
 from datasets import Dataset
@@ -16,16 +15,20 @@ import torch.nn as nn
 
 # Define the model
 
+MODEL_NAME = 'social_good_detector'
+FULL_MODEL_NAME = 'JeanMachado/social_good_detector'
+LOCAL_MODEL_NAME = 'results'
 
-def train(*, epochs: int = 10):
+def train(*, epochs: int = 10, push_model=False):
 
     if type(epochs) == str:
         epochs = int(epochs)
     print(" epochs: ", epochs)
 
     # Load the dataset from the CSV file
-    data = pd.read_csv('data.csv')
+    data = pd.read_csv('data/data.csv')
     descriptions = data['description'].tolist()
+    print('Dataset size', len(descriptions))
     labels = data['label'].tolist()
 
     # Tokenize the input data
@@ -49,6 +52,7 @@ def train(*, epochs: int = 10):
     # Split the dataset into training and validation sets
     train_size = int(0.8 * len(dataset))
     train_dataset, val_dataset = torch.utils.data.random_split(dataset, [train_size, len(dataset) - train_size])
+    print("Valuation dataset size", len(val_dataset))
 
     # Define the model
     model = AutoModelForSequenceClassification.from_pretrained('bert-base-uncased', num_labels=2)
@@ -76,16 +80,22 @@ def train(*, epochs: int = 10):
 
     # Train the model
     trainer.train()
-    trainer.save_model('./results')
+    tokenizer.save_pretrained(LOCAL_MODEL_NAME)
+    if push_model:
+        trainer.model.push_to_hub(MODEL_NAME)
+        tokenizer.push_to_hub(MODEL_NAME)
 
 
-def load_model():
-    model = AutoModelForSequenceClassification.from_pretrained('./results')
+def load_model(use_local=True):
+    model_name = LOCAL_MODEL_NAME if use_local else FULL_MODEL_NAME
+    model = AutoModelForSequenceClassification.from_pretrained(model_name)
     return model
 
-def predict(description):
+def predict(description: str =  'a startup is creating a concept to turn poverty into history', use_local=True):
+    model_name = LOCAL_MODEL_NAME if use_local else FULL_MODEL_NAME
     model = load_model()
-    tokenizer = AutoTokenizer.from_pretrained('bert-base-uncased')
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    print("predicting if it is a social good content")
     tokenized_inputs = tokenizer(description, padding=True, truncation=True, max_length=512, return_tensors='pt')
     input_ids = tokenized_inputs['input_ids']
     attention_mask = tokenized_inputs['attention_mask']
@@ -97,6 +107,3 @@ def predict(description):
 if __name__ == "__main__":
     import fire
     fire.Fire()
-
-
-
