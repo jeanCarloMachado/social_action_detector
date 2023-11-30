3702c7db2c43ab5842fc3e557a036ea9ac13a454
Jean Carlo Machado <jean.machado@getyourguide.com>
2023-12-01 00:19:10 +0100

    add llama

---
+++ llama2.py
@@ -0,0 +1,96 @@
+import torch
+
+
+def train_llama(epochs=1):
+    # Create the model
+    import torch
+    from transformers import AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig, AutoTokenizer
+
+    model = "meta-llama/Llama-2-13b-hf"
+
+    tokenizer = AutoTokenizer.from_pretrained(model, trust_remote_code=True) # device_map="auto"
+    tokenizer.pad_token = tokenizer.eos_token
+
+    bnb_config = BitsAndBytesConfig(
+        load_in_4bit=True,
+        bnb_4bit_quant_type="nf4",
+        bnb_4bit_compute_dtype=torch.float16,
+    )
+
+    model = AutoModelForCausalLM.from_pretrained(
+        model,
+        quantization_config=bnb_config,
+        trust_remote_code=True,
+        device_map="auto",   ## ADDED
+    )
+    model.config.use_cache = False
+    from peft import LoraConfig
+
+    lora_alpha = 16
+    lora_dropout = 0.05
+    lora_r = 24
+
+    peft_config = LoraConfig(
+        lora_alpha=lora_alpha,
+        lora_dropout=lora_dropout,
+        r=lora_r,
+        bias="none",
+        task_type="CAUSAL_LM",
+        target_modules=['q_proj', 'v_proj'] # Choose all linear layers from the model  'o_proj', 'k_proj',  ## 'gate_proj', 'up_proj', 'down_proj'
+    )
+
+    from transformers import TrainingArguments
+
+    output_dir = "/local_disk0/results7"
+    per_device_train_batch_size = 8
+    gradient_accumulation_steps = 2
+    optim = "paged_adamw_32bit"
+    #save_steps = 500
+    save_strategy = "no"
+    logging_steps = 20
+    learning_rate = 5e-5
+    max_grad_norm = 0.3
+    #max_steps = 50
+    warmup_ratio = 0.03
+    lr_scheduler_type = "cosine"
+    epochs = 1
+
+    training_arguments = TrainingArguments(
+        output_dir=output_dir,
+        per_device_train_batch_size=per_device_train_batch_size,
+        gradient_accumulation_steps=gradient_accumulation_steps,
+        optim=optim,
+        #save_steps=save_steps,
+        logging_steps=logging_steps,
+        learning_rate=learning_rate,
+        fp16=True,
+        max_grad_norm=max_grad_norm,    ## TODO
+        #max_steps=max_steps,
+        warmup_ratio=warmup_ratio,
+        group_by_length=True,          ## TODO
+        lr_scheduler_type=lr_scheduler_type,
+        ddp_find_unused_parameters=False,
+        num_train_epochs=epochs,
+        save_strategy=save_strategy
+    )
+    from trl import SFTTrainer
+
+    max_seq_length = 1024
+
+    trainer = SFTTrainer(
+        model=model,
+        train_dataset=dataset,
+        peft_config=peft_config,
+        dataset_text_field="text",
+        max_seq_length=max_seq_length,
+        tokenizer=tokenizer,
+        args=training_arguments,
+    )
+    for name, module in trainer.model.named_modules():
+        if "norm" in name:
+            module = module.to(torch.float32)
+    trainer.train()
+
+        # TODO: also save tokenizer?
+    trainer.model.save_pretrained("results_llama", safe_serialization=True)
+    tokenizer.save_pretrained("results_llama", safe_serialization=True)
--- main.py
+++ main.py
@@ -13,71 +13,10 @@ from datasets import Dataset
 from sklearn.metrics import accuracy_score
 import torch.nn as nn
 import config
+from bert import train_bert
+from llama2 import train_llama
 
-def train(*, epochs: int = 10):
-
-    if type(epochs) == str:
-        epochs = int(epochs)
-    print(" epochs: ", epochs)
-
-    # Load the dataset from the CSV file
-    data = pd.read_csv('data/data.csv')
-    descriptions = data['description'].tolist()
-    print('Dataset size: ', len(descriptions))
-    labels = data['label'].tolist()
-
-    # Tokenize the input data
-    tokenizer = AutoTokenizer.from_pretrained('bert-base-uncased')
-    tokenized_inputs = tokenizer(descriptions, padding=True, truncation=True, max_length=512, return_tensors='pt')
-
-    # Prepare the dataset
-    input_ids = tokenized_inputs['input_ids']
-    attention_mask = tokenized_inputs['attention_mask']
-
-    # Create a dictionary with the input data
-    data_dict = {
-        'input_ids': input_ids,
-        'attention_mask': attention_mask,
-        'labels': labels
-    }
-
-    # Create the dataset
-    dataset = Dataset.from_dict(data_dict)
-
-    # Split the dataset into training and validation sets
-    train_size = int(0.8 * len(dataset))
-    train_dataset, val_dataset = torch.utils.data.random_split(dataset, [train_size, len(dataset) - train_size])
-    print("Valuation dataset size", len(val_dataset))
-
-    # Define the model
-    model = AutoModelForSequenceClassification.from_pretrained('bert-base-uncased', num_labels=2)
-
-    # Define the training arguments
-    training_args = TrainingArguments(
-        output_dir='./results',
-        num_train_epochs=epochs,
-        per_device_train_batch_size=8,
-        per_device_eval_batch_size=8,
-        warmup_steps=500,
-        weight_decay=0.01,
-        logging_dir='./logs',
-        logging_steps=10,
-        evaluation_strategy='epoch'
-    )
-
-    # Define the trainer
-    trainer = Trainer(
-        model=model,
-        args=training_args,
-        train_dataset=train_dataset,
-        eval_dataset=val_dataset,
-    )
-
-    # Train the model
-    trainer.train()
-    trainer.model.save_pretrained(config.LOCAL_MODEL_NAME)
-    tokenizer.save_pretrained(config.LOCAL_MODEL_NAME)
-    print('Model trained and saved locally in the "results" folder!')
+train = train_bert
 
 def push_to_hub():
     model, tokenizer = load_model()
