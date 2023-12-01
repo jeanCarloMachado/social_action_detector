704550dd43f02af6a44c922d11b11023170ce1e5
Jean Carlo Machado <jean.machado@getyourguide.com>
2023-12-01 22:53:08 +0100

    test predict

--- social_action_detector/main.py
+++ social_action_detector/main.py
@@ -9,7 +9,7 @@ from social_action_detector.llama2 import train_llama
 train = train_bert
 
 def push_to_hub():
-    model, tokenizer = load_model()
+    model, tokenizer = load_model(config.LOCAL_MODEL_NAME)
     model.push_to_hub(config.MODEL_NAME)
     tokenizer.push_to_hub(config.MODEL_NAME)
     print('Model pushed to the hub!')
@@ -19,17 +19,20 @@ def push_data_to_hub():
 
     dataset = get_dataset()
 
-def load_model(use_local=True):
-    model_name = config.LOCAL_MODEL_NAME if use_local else config.FULL_MODEL_NAME
+def load_model(model_name):
     print("loading model: ", model_name)
     tokenizer = AutoTokenizer.from_pretrained(model_name)
     model = AutoModelForSequenceClassification.from_pretrained(model_name)
     return model, tokenizer
 
-def predict(description: str =  'a startup is creating a concept to turn poverty into history', use_local=True):
-    model_name = config.LOCAL_MODEL_NAME if use_local else config.FULL_MODEL_NAME
-    model, tokenizer = load_model()
-    print("predicting if it is a social good content")
+def predict(description: str =  'a startup is creating a concept to turn poverty into history', model = None, tokenizer = None):
+
+    if model is None or tokenizer is None:
+        model_name = config.LOCAL_MODEL_NAME
+        model, tokenizer = load_model(model_name)
+
+    print("predicting if it is a social good content for model ", model_name)
+
     tokenized_inputs = tokenizer(description, padding=True, truncation=True, max_length=512, return_tensors='pt')
     input_ids = tokenized_inputs['input_ids']
     attention_mask = tokenized_inputs['attention_mask']
